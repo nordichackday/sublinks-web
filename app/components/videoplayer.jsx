12260063fbfaadb0531425d24f35b167a06b683a
Sebastian Suarez-Golborne <sesu01@Sebastians-MacBook-Pro.local>
2015-05-29 11:10:27 +0200

    jpjp

--- app/components/videoplayer.jsx
+++ app/components/videoplayer.jsx
@@ -3,6 +3,8 @@
 var React = require('react')
 
 var videoid = decodeURIComponent(window.location.search.substring(1).split("&")[0].split("=")[1]);
+var videotype = decodeURIComponent(window.location.search.substring(1).split("&")[1].split("=")[1]);
+
 
 module.exports = React.createClass({
     displayName: 'VideoPlayer',
@@ -13,7 +15,7 @@ module.exports = React.createClass({
 
 
     <div dangerouslySetInnerHTML={
-      {__html: '<iframe width="100%" height="700" src="http://www.svtplay.se/klipp/' + videoid + '?type=embed" allowfullscreen="allowfullscreen" frameborder="0"></iframe>'}
+      {__html: '<iframe width="100%" height="700" src="http://www.svtplay.se/' + videotype + '/' + videoid + '?type=embed" allowfullscreen="allowfullscreen" frameborder="0"></iframe>'}
       }
        />
 
